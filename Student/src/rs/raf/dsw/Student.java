390b8e494ccb1e048823211825a83229e46e5a78
Zarko <zarkoned@outloo.com>
2024-10-08 13:10:52 +0200

    added get full name method

--- Student/src/rs/raf/dsw/Student.java
+++ Student/src/rs/raf/dsw/Student.java
@@ -8,4 +8,14 @@ public class Student {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    // Metod koji ćemo menjati u različitim granama
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("Žarko", "Nedeljković");
+        System.out.println(student.getFullName());
+    }
 }
