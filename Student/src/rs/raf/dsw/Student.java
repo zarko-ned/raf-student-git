d0a1d250b6882e037edbd1098820fe094e99b897
znedeljkovic <znedeljkovic@raf.rs>
2024-10-08 15:46:29 +0200

    added address to student

--- Student/src/rs/raf/dsw/Student.java
+++ Student/src/rs/raf/dsw/Student.java
@@ -3,10 +3,12 @@ package rs.raf.dsw;
 public class Student {
     private String firstName;
     private String lastName;
+    private String address;
 
-    public Student(String firstName, String lastName) {
+    public Student(String firstName, String lastName, String address) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.address = address;
     }
 
     // Metod koji ćemo menjati u različitim granama
@@ -15,7 +17,7 @@ public class Student {
     }
 
     public static void main(String[] args) {
-        Student student = new Student("Žarko", "Nedeljković");
+        Student student = new Student("Žarko", "Nedeljković", "Knez");
         System.out.println(student.getFullName());
     }
 }
